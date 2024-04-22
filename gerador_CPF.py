ef9602214ee493688662f41a0489814c42db4978
Zé Mário <josemariofr@live.com>
2024-04-22 19:53:44 -0300

    gerador_CPF

--- gerador_CPF.py
+++ gerador_CPF.py
@@ -1,5 +1,8 @@
 import random
 import tkinter as tk
+import pyperclip
+
+cpf_history = []
 
 def generate_cpf():
     # Gerando os primeiros 9 dígitos do CPF
@@ -29,20 +32,38 @@ def generate_cpf():
 
     # Interface gráfica do CPF
     result_label.config(text=cpf_str)
+    pyperclip.copy(cpf_str)  # Copy the generated CPF to the clipboard
+
+    # Adicionando histórico dos CPF´s gerados
+    cpf_history.append(cpf_str)
+
+def show_cpf_history():
+    # Display do histórico
+    history_window = tk.Toplevel(root)
+    history_window.title("Histórico de CPFs")
+
+    history_label = tk.Label(history_window, text="\n".join(cpf_history))
+    history_label.pack()
 
 # Criando o menu principal
 root = tk.Tk()
 root.title("Gerador de CPF")
 
-# # Cria um campo para exibir o CPF
-# entry_label = tk.Label(root, text="Number of CPFs to generate:")
-# entry_label.pack()
-# entry = tk.Entry(root)
-# entry.pack()
-
 # Criando o botão pra gerar CPF
 generate_button = tk.Button(root, text="Gerar", command=generate_cpf)
 generate_button.pack()
+
+# Criando o botão pra copiar o CPF
+copy_button = tk.Button(root, text="Copiar", command=lambda: pyperclip.copy(result_label.cget("text")))
+copy_button.pack()
+
+# Criando o menu para exibir o histórico
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+history_menu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Histórico", menu=history_menu)
+history_menu.add_command(label="Exibir histórico", command=show_cpf_history)
 
 # Gerar o resultado
 result_label = tk.Label(root, text="")
